55e544e6dd0667645d6889aaae72927d6a0de2f8
Orxan Qanbarov <41814449+Orxan477@users.noreply.github.com>
2021-11-14 15:46:15 +0400

    Summary added

--- FolderTask/Program.cs
+++ FolderTask/Program.cs
@@ -7,8 +7,10 @@ namespace FolderTask
     {
         static void Main(string[] args)
         {
+            //  We find the current location of the project
             string CurrentDirectory = Environment.CurrentDirectory;
             DirectoryInfo TestingInfo = new DirectoryInfo(@"Testing");
+            // We will check if there is a Testing folder, delete it and re-create it.
             if (TestingInfo.Exists)
             {
                 Directory.Delete(TestingInfo.FullName, true);
@@ -17,10 +19,12 @@ namespace FolderTask
             }
             Directory.CreateDirectory(@"Testing");
             Console.WriteLine("'Testing' Folder Created");
+            // We create folders in the test folder.
             Directory.CreateDirectory(@"Testing/FolderCheck1/FolderCheck2/FolderCheck3");
             Console.WriteLine("--------------------------------------");
             Console.WriteLine("'FolderCheck1', 'FolderCheck2' and 'FolderCheck3' Folders Created");
             Console.WriteLine("-----------------------------------------------------------------------");
+            // We create files in the folders we created in the test folder.
             FileStream fileStream = File.Create(@"Testing/OrxanAzTU.html");
             fileStream.Close();
             FileStream fileStream1 = File.Create(@"Testing/FolderCheck1/Check1.html");
@@ -31,21 +35,28 @@ namespace FolderTask
             fileStream3.Close();
             Console.WriteLine("'OrxanAzTU.html', 'Check1.html', 'Check2.txt' and 'Check3.txt' Files Created");
             Console.WriteLine("---------------------------------------------------------------------------------");
+            // We specify the location of the folder to be copied.
             string NewDirectory = Path.Combine(CurrentDirectory, "TestingCopy");          
             DirectoryInfo NewDirectoryInfo = new DirectoryInfo(NewDirectory);
+            // If we check if there is a folder to be copied, we delete it and re-create it.
             if (NewDirectoryInfo.Exists)
             {                
                 Directory.Delete(NewDirectoryInfo.FullName,true);
                 Console.WriteLine("'TestingCopy' Folder Removed");
                 Console.WriteLine("---------------------------------");
             }          
-            Console.WriteLine("'TestingCopy' Folder Created");
             Directory.CreateDirectory(NewDirectoryInfo.FullName);
+            Console.WriteLine("'TestingCopy' Folder Created");
             CopyRekursiv("Testing", NewDirectory);
             Console.WriteLine("-----------------------------------------------------");
             Console.WriteLine("Directorys and Files Copy Completed");
             Console.WriteLine("-------------------------------------------\nFinished\n-----------------------");
         }
+        /// <summary>
+        /// Method for copying folders and files
+        /// </summary>
+        /// <param name="current">Includes where the project is currently located</param>
+        /// <param name="newdirectory">Includes the location of the new folder where the project is currently located</param>
         public static void CopyRekursiv(string current, string newdirectory)
         {
             //Folder
